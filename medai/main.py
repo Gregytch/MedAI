601bbdbf11ce1926ea2fe734ff1c7b88c996df47
Gregoire Reuter <gregoire.reuter@gmail.com>
2025-01-29 12:05:10 +0100

    applying all changes seen with TAs in code an structure

--- medai/main.py
+++ medai/main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
-
+import os
 
 from models.data import clean_data
 
 def preprocess():
-
+    file=__file__
+    dir=os.path.dirname(file)
     # Load data from directory (relatve path)
-    df_symp = pd.read_csv('/home/greg_ytch/code/Gregytch/MedAI/raw_data/Final_Augmented_dataset_Diseases_and_Symptoms.csv')
+    df_symp = pd.read_csv(os.path.join(dir,'../raw_data/Final_Augmented_dataset_Diseases_and_Symptoms.csv'))
 
     #Clean the data calling the function clean_data
     data=clean_data(df_symp)
