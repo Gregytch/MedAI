085ecd5513edb4851458d088d4247c9729fdaf45
Gregoire Reuter <gregoire.reuter@gmail.com>
2025-01-30 11:57:41 +0100

    predict function with loading model and encoder from pickle, input hardcoded, returns df with top ten diseases

--- medai/main.py
+++ medai/main.py
@@ -3,11 +3,13 @@ import pandas as pd
 import os
 
 from ml_logic.data import clean_data
+from ml_logic.registry import load_model
+from sklearn.preprocessing import LabelEncoder
 
 def preprocess():
     file=__file__
     dir=os.path.dirname(file)
-    # Load data from directory (relatve path)
+    # Load data from directory (relative path)
     df_symp = pd.read_csv(os.path.join(dir,'../raw_data/Final_Augmented_dataset_Diseases_and_Symptoms.csv'))
 
     #Clean the data calling the function clean_data
@@ -27,5 +29,34 @@ def preprocess():
 
     return data, X, y
 
+def pred(X_pred) :
+
+    print( "\n ⭐️ predicting diseases" )
+
+
+    #load the model
+    model, label_encoder = load_model()
+
+    #predict the most likely diseases
+    probs=model.predict_proba(X_pred)[0]
+
+    #create dataframe with results
+    df_probs = pd.DataFrame({
+        "Disease": label_encoder.classes_,  # List of disease names
+        "Probability": probs   # Corresponding probabilities
+    })
+
+    #sorting by highest proba
+    df_probs_sorted = df_probs.sort_values(by="Probability", ascending=False).reset_index(drop=True)
+
+    #print results
+    print(f"✅ pred() done")
+    print(f"🏥 To ten predicted disease with probability: {df_probs_sorted[0:10]}")
+
+    return df_probs_sorted
+
+
+
 if __name__ == '__main__':
-    preprocess()
+    data,X,y=preprocess()
+    pred(X[0:1])
