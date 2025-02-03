5d36656fb14e7951b33b40dd5cb1bc9c55609b36
Ricarda Schaefer <ricarda@schaefervoigt.de>
2025-02-03 16:43:47 +0100

    Docker on the cloud integrated

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3.10.6-buster
 
 COPY medai medai
+COPY models models
 COPY requirements_prod.txt requirements_prod.txt
 
 RUN pip install --upgrade pip
 RUN pip install -r requirements_prod.txt
 
-CMD uvicorn medai.api.fast:app --host 0.0.0.0
+CMD uvicorn medai.api.fast:app --host 0.0.0.0 --port $PORT
--- medai/ml_logic/registry.py
+++ medai/ml_logic/registry.py
@@ -17,7 +17,7 @@ def load_model(stage="Production"):
 
     ##RELATIVE DIRECTORY
     dir=os.path.dirname(__file__)
-    MODEL_PATH = os.path.join(dir, "../../models/XGB_model.pkl")
+    MODEL_PATH = os.path.join(dir, "../../models/xgb_model_full.pkl")
     ENCODER_PATH = os.path.join(dir, "../../models/label_encoder.pkl")
 
     ## Load model from directory (works when same directory, otherwise add relative phat above path)
