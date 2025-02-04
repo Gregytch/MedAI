da56026dbd8c3cddf05864b91e6d92f57e897e49
Gregoire Reuter <gregoire.reuter@gmail.com>
2025-02-04 11:59:36 +0100

    output changed, functions optimized

--- medai/api/fast.py
+++ medai/api/fast.py
@@ -1,10 +1,5 @@
-import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import HTTPException, Depends
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel, Field
-from typing import List, Dict, Any
 from medai.main import runthrough_api
 
 
@@ -27,26 +22,9 @@ def root():
     'greeting': 'Hello, this is the first test'
     }
 
-class DataRow(BaseModel):
-    data: List[Dict[str, Any]] = Field(..., description="A list of dictionaries, where each dictionary represents a row with column names as keys and numeric values.")
-
-@app.post('/predict_disease')
-def get_prediction(row: DataRow):
-    df = pd.DataFrame(row.model_dump()["data"])
-
-# Do some processing (modify as needed)
-
-    res1 = df.sum(axis=1, numeric_only=True)
-    res2 = df.mean(axis=1, numeric_only=True)
-    return {"result1": f"Here is prediction 1: {res1[0]}", "result2": f"Here is prediction2: {res2[0]}"}
 
 
 @app.get('/diagnosis')
 def get_diagnosis(inputs: str):
-    df = runthrough_api(inputs) # adapt function??
-    output = {}
-    predictions = []
-    for i in range(10): #Check if 10 is length of DF
-        predictions.append(df.iloc[i].to_dict()) # probability checker??
-    output["predictions"] = predictions
+    output = runthrough_api(inputs) # adapt function??
     return output
